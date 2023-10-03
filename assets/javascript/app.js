730ecdd17ff4e933759ea1b376db9aaf3fd5c6a1
Erick Coelho <erickcoelho06@gmail.com>
2023-10-03 20:23:37 -0300

    POST and GET messages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,37 +4,52 @@ import dayjs from 'dayjs';
 import joi from 'joi';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
+
+// express configuration
+
 dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+const PORT = process.env.PORT || 5001;
+const MONGO_URI = process.env.MONGO_URI;
+const DB_NAME = 'chat_zap_db'
+
 
 const participantSchena = joi.object({
     name: joi.string().required()
 });
 
-// mongo configuration
-const mongoClient = new MongoClient(process.env.MONGO_URI);
-let db;
-mongoClient.connect(() => {
-    db = mongoClient.db("chat_zap_db");
+const messageSchena = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.any().valid('message', 'private_message')
 });
 
-async function mongoConnection() {
+// mongo configuration
+
+let db;
+
+async function connectToMongo() {
     try {
+        const mongoClient = new MongoClient(MONGO_URI);
         await mongoClient.connect();
         console.log('Conexão com o MongoDB estabelecida com sucesso');
-        db = mongoClient.db("chat_zap_db");
+        db = mongoClient.db(DB_NAME);
     } catch (error) {
         console.error('Erro ao conectar ao MongoDB:', error);
     }
 }
 
-// express configuration
-const app = express();
-app.use(express.json());
-app.use(cors());
+
+//participants
 
 app.get('/participants', async (req, res) => {
     try {
-        await mongoConnection();
+        await connectToMongo();
         const participants = await db.collection('participants').find().toArray();
         res.send(participants);
     } catch (error) {
@@ -46,23 +61,24 @@ app.get('/participants', async (req, res) => {
 app.post('/participants', async (req, res) => {
     const participant = req.body;
     const validation = participantSchena.validate(participant, { abortEarly: true });
+
     if (validation.error) {
-        res.send(422);
-        console.log(validation.error.details);
+        res.status(422).send(validation.error.details);
         return;
     }
 
     try {
-        await mongoConnection();
-        const existingParticipant = await db.collection('participants').findOne({ _name: req.body.name });
-        if(!existingParticipant) {
-        const insertParticipant = { ...participant, lastStatus: Date.now() };
-        await db.collection('participants').insertOne(insertParticipant);
+        await connectToMongo();
+        const existingParticipant = await db.collection('participants').findOne({ name: req.body.name });
 
-        const time = dayjs(Date.now()).format('HH:mm:ss');
-        const message = {from: req.body.name,  to: 'Todos', text: 'entra na sala...', type: 'status', time: time};
-        await db.collection('messages').insertOne(message);
-        res.sendStatus(201);
+        if (!existingParticipant) {
+            const insertParticipant = { ...participant, lastStatus: Date.now() };
+            await db.collection('participants').insertOne(insertParticipant);
+
+            const time = dayjs(Date.now()).format('HH:mm:ss');
+            const message = { from: req.body.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: time };
+            await db.collection('messages').insertOne(message);
+            res.sendStatus(201);
         }
         else {
             res.sendStatus(409);
@@ -73,9 +89,49 @@ app.post('/participants', async (req, res) => {
     }
 });
 
-//app.get('/messages', (req, res) => {});
 
-//app.post('/messages', (req, res) => {});
+//messages
+
+app.get('/messages', async (req, res) => {
+    try {
+        await connectToMongo();
+        const messages = await db.collection('messages').find().toArray();
+        res.send(messages);
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+});
+
+app.post('/messages', async (req, res) => {
+    const messageBody = req.body;
+    const messageFrom = req.header.User;
+
+    const validation = messageSchena.validate(messageBody, { abortEarly: true });
+    if (validation.error) {
+        res.status(422).send(validation.error.details);
+        return;
+    }
+
+    try {
+        await connectToMongo();
+
+        const existingParticipant = await db.collection('participant').findOne({ name: messageFrom });
+
+        if (!existingParticipant) {
+            res.sendStatus(422);
+        }
+        else {
+            const time = dayjs(Date.now()).format('HH:mm:ss');
+            const message = { ...messageBody, from: messageFrom, time: time };
+            await db.collection('messages').insertOne(message);
+            res.sendStatus(201);
+        }
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+});
 
 //app.put('/messages', (req, res) => {});
 
@@ -83,6 +139,6 @@ app.post('/participants', async (req, res) => {
 
 //app.post('/status', (req, res) => {});
 
-app.listen(5001, () => {
+app.listen(PORT, () => {
     console.log('Server is listening on port 5001.');
 });
